ba3cfeb940b7ea256c873085e14750ed760944e5
AnuragVV <anuragvvworkspace@gmail.com>
2023-10-22 14:41:53 -0700

    push takes for ever

--- Tutorials/Basics/feed_forward_networks/main.py
+++ Tutorials/Basics/feed_forward_networks/main.py
@@ -10,7 +10,6 @@ print(device)
 batch_size = 64;
 no_of_epoch = 10;
 
-
 # train
 # Feed forward network
 train_dataset = torchvision.datasets.MNIST(root = '../../data/', train = True, download = True);
@@ -25,6 +24,9 @@ image,_ = next(data_iter);
 
 #print(image.size()[2]*image.size()[3]);
 input_size = image.size()[2]*image.size()[3];
+print("size of image :" , input_size);
+print("Lenght of datset:",len(train_loader.dataset));
+
 
 class ffnet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
@@ -38,6 +40,24 @@ class ffnet(nn.Module):
         out = self.relu(out);
         out = self.fc2(out);
         return out;
+
+# Test modelimages
+# To save compute, gradients are turned off:
+mynet = ffnet(28*28, 10, 10);
+with torch.no_grad():
+    correct = 0;
+    total = 0;
+    for images,labels in test_loader:
+        pred = mynet(images.reshape(-1,input_size));
+        #print(pred[0]);
+        #print(labels.size());
+        _,predicted = torch.max(pred.data,1);
+
+        total+=labels.size(0);
+        correct+=(predicted == labels).sum().item();
+print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+
+
 
 
         
